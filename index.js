3cb8eb3c7127fc8c02dbfa987693d7f696687d47
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2019-06-30 23:42:05 -0500

    Principales rutas Rest en express

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Word');
+// Para uso de JSON´s
+app.use(express.json());
+
+app.get('/user', (req, res) => {
+    res.json({
+        'username': 'Cameron',
+        'lastname': 'Howe'
+    });
+});
+
+// Obtención de los parametros de la url
+app.post('/user/:id', (req, res) => {
+    console.log(req.params);
+    res.send('Hello post');
+});
+
+app.post('/user', (req, res) => {
+    // Obtención del curpo de la petición
+    console.log(req.body);
+    res.send('Hello post');
+});
+
+app.put('/user', (req, res) => {
+    res.send('Hello put');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Hello delete');
 });
 
 app.listen(3000, () => {
